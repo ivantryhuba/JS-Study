c8f1bc16eb7b9a41acefaf5108def68e689a1921
Ivan Tryhuba <ivantriguba@i.ua>
2021-04-02 18:16:07 +0300

    Update js-5.js

--- js/js-5.js
+++ js/js-5.js
@@ -149,7 +149,6 @@
 // Объявление класса
 // class Car{};
 
-
 // ************Задача 9 - 5
 // Конструктор класса
 // class Car {
@@ -163,7 +162,6 @@
 // const audi = new Car({ brand: 'Audi', model: 'Q3', price: 36000 });
 // console.log(audi);
 
-
 // ************Задача 10 - 5
 // Методы класса
 // class Car {
@@ -172,11 +170,11 @@
 //     this.model = model;
 //     this.price = price;
 //   };
-  
-  // getPrice(){
-  // return this.price;
-  // };
-  
+
+// getPrice(){
+// return this.price;
+// };
+
 //   changePrice(newPrice){
 //   this.price=newPrice;
 //   };
@@ -188,21 +186,20 @@
 // audi.changePrice(40000);
 // console.log(audi.getPrice());
 
-
 // ************Задача 11 - 5
 // Приватные свойства
 // class Car {
 //   #brand;
-//   constructor({ brand, model, price }) {
-//     this.#brand = brand;
-//     this.model = model;
-//     this.price = price;
-//   }
-  
+// constructor({ brand, model, price }) {
+//   this.#brand = brand;
+//   this.model = model;
+//   this.price = price;
+// }
+
 //   getBrand(){
 //   return this.#brand;
 //   };
-  
+
 //   changeBrand(newBrand){
 //   this.#brand=newBrand;
 //   };
@@ -213,3 +210,296 @@
 // console.log(audi.getBrand());
 // audi.changeBrand('Tesla');
 // console.log(audi.getBrand());
+
+// ************Задача 12 - 5
+// Задача. Хранилище 2.0
+// class Storage {
+//   #items;
+
+//   constructor(items) {
+//       this.#items = items;
+//   }
+
+// getItems() {
+//   return this.#items;
+// };
+
+// addItem(newItem) {
+//   this.#items.push(newItem);
+// };
+
+// removeItem(item) {
+//   const itemIndex = this.#items.indexOf(item);
+//   this.#items.splice(itemIndex, 1);
+// };
+
+// };
+// // Пиши код выше этой строки
+// const storage = new Storage(["Нанитоиды", "Пролонгер", "Антигравитатор"]);
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор"]
+// storage.addItem("Дроид");
+// console.log(storage.getItems()); // ["Нанитоиды", "Пролонгер", "Антигравитатор", "Дроид"]
+// storage.removeItem("Пролонгер");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Дроид"]
+
+// ************Задача 13 - 5
+// Задача. Конструктор строк 2.0
+// class StringBuilder {
+//   #value
+
+//   constructor(baseValue){
+//   this.#value = baseValue;
+// }
+
+// getValue() {
+//   return this.#value;
+// };
+
+// padEnd(str) {
+//   this.#value += str;
+// };
+
+// padStart(str) {
+//   this.#value = str + this.#value;
+// };
+
+// padBoth(str) {
+//   this.padStart(str);
+//   this.padEnd(str);
+// };
+// };
+
+// // Пиши код выше этой строки
+// const builder = new StringBuilder('.');
+// console.log(builder.getValue()); // '.'
+// builder.padStart('^');
+// console.log(builder.getValue()); // '^.'
+// builder.padEnd('^');
+// console.log(builder.getValue()); // '^.^'
+// builder.padBoth('=');
+// console.log(builder.getValue()); // '=^.^='
+
+// ************Задача 14 - 5
+// Геттеры и сеттеры
+// class Car {
+//   #model;
+//   #price;
+//   #brand;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+// set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+// }
+
+// ************Задача 15 - 5
+// Статические свойства
+// class Car {
+//   // Пиши код ниже этой строки
+//   static MAX_PRICE = 50000;
+//   #price;
+
+//   constructor({ price }) {
+//     this.#price = price;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     if(newPrice > Car.MAX_PRICE){
+//     return;
+//     }
+//     this.#price = newPrice;
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// const audi = new Car({price: 35000});
+// console.log(audi.price); // 35000
+// audi.price = 49000;
+// console.log(audi.price); // 49000
+// audi.price = 51000;
+// console.log(audi.price); // 49000
+
+// ************Задача 16 - 5
+// Статические методы
+// class Car {
+//   static #MAX_PRICE = 50000;
+//   // Пиши код ниже этой строки
+
+//   static checkPrice(price) {
+//     if (price > Car.#MAX_PRICE) {
+//       return "Внимание! Цена превышает допустимую.";
+//     }
+//     return "Всё хорошо, цена в порядке.";
+//   }
+
+//   // Пиши код выше этой строки
+//   constructor({ price }) {
+//     this.price = price;
+//   }
+// }
+
+// const audi = new Car({ price: 36000 });
+// const bmw = new Car({ price: 64000 });
+// console.log(Car.checkPrice(audi.price)); // Всё хорошо, цена в порядке.
+// console.log(Car.checkPrice(bmw.price)); // Внимание! Цена превышает допустимую.
+
+
+// ************Задача 17 - 5
+// Наследование классов
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// // Пиши код ниже этой строки
+
+// class Admin extends User {
+//   static AccessLevel = { 
+//    BASIC: 'basic', 
+//    SUPERUSER: 'superuser' 
+//   }
+// }
+
+// console.log(Admin.AccessLevel.BASIC);
+// console.log(Admin.AccessLevel.SUPERUSER);
+
+
+// ************Задача 18 - 5
+// Конструктор дочернего класса
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+
+// class Admin extends User {
+//   // Пиши код ниже этой строки
+// 	accessLevel;
+  
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser'
+//   };
+
+//   constructor({email, accessLevel}) {
+//     super(email);
+
+//   }
+//   // Пиши код выше этой строки
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER
+// });
+
+// console.log(mango.email); // mango@mail.com
+// console.log(mango.accessLevel); // superuser
+
+
+// ************Задача 19 - 5
+// Методы дочернего класса
+// class User {
+//   email;
+
+//   constructor(email) {
+//     this.email = email;
+//   }
+
+//   get email() {
+//     return this.email;
+//   }
+
+//   set email(newEmail) {
+//     this.email = newEmail;
+//   }
+// }
+// class Admin extends User {
+//   // Пиши код ниже этой строки
+
+//   static AccessLevel = {
+//     BASIC: 'basic',
+//     SUPERUSER: 'superuser'
+//   };
+
+//   accessLevel;
+//    blacklistedEmails;
+
+//   constructor({ email, accessLevel,  blacklistedEmails}) {
+//     super(email);
+//     this.accessLevel = accessLevel;
+//     this.blacklistedEmails=[];
+//   }
+
+//   blacklist(email){
+//   this.blacklistedEmails.push(email);
+//   };
+  
+//   isBlacklisted(email){
+//   if(this.blacklistedEmails.includes(email)){
+//   return true;}
+//   return false;
+//   };
+//   // Пиши код выше этой строки
+// }
+
+// const mango = new Admin({
+//   email: 'mango@mail.com',
+//   accessLevel: Admin.AccessLevel.SUPERUSER
+// });
+
+// console.log(mango.email); // mango@mail.com
+// console.log(mango.accessLevel); // superuser
+// mango.blacklist('poly@mail.com');
+// console.log(mango.blacklistedEmails); // 'poly@mail.com'
+// console.log(mango.isBlacklisted('mango@mail.com')); //  false
+// console.log(mango.isBlacklisted('poly@mail.com')); // true 
